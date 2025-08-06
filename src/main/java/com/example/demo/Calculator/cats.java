ceda61956ca9b154bac19cec6e00e932af6dbe00
yumna <yumnashabbir2@gmail.com>
2025-08-06 12:15:27 +0500

    a new logic again?

--- src/main/java/com/example/demo/Calculator/cats.java
+++ src/main/java/com/example/demo/Calculator/cats.java
@@ -3,12 +3,16 @@ package com.example.demo.Calculator;
 public class cats {
     String name;
     int age;
+    String breed;
 
     public void catName(String name){
         System.out.println("Cat name is " + name);
     }
     public void catAge(int age){
         System.out.println("Cat age is " + age);
+    }
+    public void catBreed(String breed){
+        System.out.println("Cat breed is " + breed);
     }
 
 
